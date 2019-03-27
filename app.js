d1018e1e8c811b5cdd718aa679871717813e6bc0
yaozhixing <yaozhixing797600@163.com>
2019-03-28 01:19:21 +0800

    app init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = (process.env.PORT || 3000);
+
+//静态加载文件
+app.use('/public', express.static('public'));
+
+//设置views的html文件
+app.set('views',__dirname + '/src/views');
+
+
+//模板引擎
+app.set("view engine", "ejs");
+
+//路由
+app.get("/", (req, res) =>{
+    res.render('index');
+})
+
+app.listen(port, ()=>{
+    console.log("likeread is runing port:3000...")
+})
