6924dd4f023b05359887efd6a380885720eff9cd
yaozhixing <yaozhixing797600@163.com>
2019-03-30 01:51:31 +0800

    渲染视图层

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const ejs = require('ejs');
 const app = express();
+const { books } = require("./public/json/books.json");
 const port = (process.env.PORT || 3000);
 
 //静态加载文件
@@ -10,11 +12,22 @@ app.set('views',__dirname + '/src/views');
 
 
 //模板引擎
-app.set("view engine", "ejs");
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
 
 //路由
 app.get("/", (req, res) =>{
-    res.render('index');
+    res.render('index',{
+        title: '我爱阅读官网',
+        books: books
+    });
 })
+
+app.get("/login", (req, res) =>{
+    res.render('login', {
+        title: '登录页面'
+    });
+})
+
 
 app.listen(port);
